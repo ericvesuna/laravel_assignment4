48bc06ae2f718a9a59831c8e7b0e71c58c6bc061
Neosoft <neosoft@IT09-107>
2019-09-03 18:13:23 +0530

    category add, update, delete and multiple delete

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Route::get('/', function () {
+#     return view('pages.list_category');
+# });
 
-Route::get('/templates', function () {
+Route::get('/', 'CategoryController@index');
+Route::get('/createcategory', function () {
 	return view('pages.create_category');
 	});
+
+
+
+Route::post('add_category', 'CategoryController@store')->name('add_category');
+
+Route::get('single_delete','CategoryController@destroy')->name('single_delete');
+
+Route::post('multiple_delete_category', 'CategoryController@destroy_all')->name('multiple_delete_category');
