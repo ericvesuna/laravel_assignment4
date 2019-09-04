cc893ca21e5bdd4640ac098fd7de1f4a40c7c53b
Neosoft <neosoft@IT09-107>
2019-09-04 17:26:55 +0530

    complete working category module along with product list and create page

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,19 @@
 |
 */
 
-# Route::get('/', function () {
-#     return view('pages.list_category');
-# });
+ Route::get('/', function () {     
+ return view('pages.select');
+});
 
-Route::get('/', 'CategoryController@index');
+Route::get('/categorylist', 'CategoryController@index')->name('categorylist');
+
 Route::get('/createcategory', function () {
 	return view('pages.create_category');
 	});
 
+Route::get('/productlist', function () {
+	return view('pages.list_product');
+	});
 
 
 Route::post('add_category', 'CategoryController@store')->name('add_category');
@@ -27,3 +31,12 @@ Route::post('add_category', 'CategoryController@store')->name('add_category');
 Route::get('single_delete','CategoryController@destroy')->name('single_delete');
 
 Route::post('multiple_delete_category', 'CategoryController@destroy_all')->name('multiple_delete_category');
+
+Route::get('edit_category', 'CategoryController@edit')->name('edit_category');
+
+Route::post('update_category/{id}', 'CategoryController@update')->name('update_category');
+
+
+Route::get('createproduct', function () {
+	return view('pages.create_product');
+	});
